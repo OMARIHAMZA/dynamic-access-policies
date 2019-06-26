8b5228eff78818ad7cc3921bb01cda4a06883527
OMARIHAMZA <36658089+OMARIHAMZA@users.noreply.github.com>
2019-06-26 13:52:27 +0300

    emergency access history migration

---
+++ database/migrations/2019_06_26_105124_emergency_access_history.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class EmergencyAccessHistory extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('emergency_access_history', function (Blueprint $table) {
+            $table->bigInteger('requester_id');
+            $table->json('external_tables');
+            $table->timestamp('access_date');
+            $table->foreign('requester_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('emergency_access_history');
+    }
+}
