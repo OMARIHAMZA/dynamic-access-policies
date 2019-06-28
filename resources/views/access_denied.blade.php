a74e2c83d0c97ca09d02e9bba23a5594d16e017c
Nazir Allaham <nazeer12allaham@gmail.com>
2019-06-28 03:10:35 +0300

    Login Page

--- resources/views/access_denied.blade.php
+++ resources/views/access_denied.blade.php
@@ -1,29 +1,25 @@
 <style>
-    #parent, #parent * {
+    #root * {
         -webkit-box-sizing: border-box;
         box-sizing: border-box;
     }
 
-    #parent {
+    #root {
         padding: 0;
         margin: 0;
+        width: 100%;
+        height: 100%;
+
     }
 
-    #error-403 {
+    #root .container {
+        width: 100%;
+        height: 100%;
+        padding: 28px 48px;
         position: relative;
-        height: 100vh;
     }
 
-    #error-403 .error-403 {
-        position: absolute;
-        left: 50%;
-        top: 50%;
-        -webkit-transform: translate(-50%, -50%);
-        -ms-transform: translate(-50%, -50%);
-        transform: translate(-50%, -50%);
-    }
-
-    .error-403 {
+    #root .container .background {
         max-width: 920px;
         width: 100%;
         line-height: 1.4;
@@ -32,23 +28,23 @@
         padding-right: 15px;
     }
 
-    .error-403 .error-403 {
+    #root .container .background .error-403 {
         position: absolute;
-        height: 100px;
+        height: 200px;
         top: 0;
         left: 50%;
         -webkit-transform: translateX(-50%);
         -ms-transform: translateX(-50%);
         transform: translateX(-50%);
-        z-index: -1;
+        z-index: 1;
     }
 
-    .error-403 .error-403 h1 {
-        font-family: 'Arial Rounded MT Bold', sans-serif;
+    #root .container .background .error-403 h1 {
+        font-family: 'Century Gothic', sans-serif;
         color: #aabbcc;
         font-weight: 900;
         font-size: 276px;
-        margin: 0px;
+        margin: 0;
         position: absolute;
         left: 50%;
         top: 50%;
@@ -57,87 +53,135 @@
         transform: translate(-50%, -50%);
     }
 
-    .error-403 h2 {
-        font-family: 'Arial Rounded MT Bold', sans-serif;
+    #root .container .foreground {
+        max-width: 920px;
+        width: 100%;
+        line-height: 1.4;
+        text-align: center !important;
+        padding-left: 15px;
+        padding-right: 15px;
+        padding-top: 128px;
+        margin: 0 auto !important;
+        z-index: 12;
+    }
+
+    #root .container .foreground > div {
+
+    }
+
+    #root .container .foreground h2 {
+        font-family: 'Century Gothic', sans-serif;
         font-size: 46px;
         color: #000;
         font-weight: 900;
         text-transform: uppercase;
-        margin: 0px;
+        margin: 0;
+        z-index: 3;
     }
 
-    .error-403 h4 {
-        font-family: 'Arial Rounded MT Bold', sans-serif;
+    #root .container .foreground h4 {
+        font-family: 'Century Gothic', sans-serif;
         font-size: 32px;
         color: #000;
         font-weight: 600;
         /*text-transform: uppercase;*/
-        margin: 0px;
+        margin: 0;
+        z-index: 3;
     }
 
-    .error-403 p {
-        font-family: 'Arial Rounded MT Bold', sans-serif;
+    #root .container .foreground p {
+        font-family: 'Century Gothic', sans-serif;
         font-size: 16px;
         color: #000;
         font-weight: 400;
         text-transform: uppercase;
         margin-top: 15px;
+        z-index: 3;
     }
 
-    .error-403 a {
-        font-family: 'Arial Rounded MT Bold', sans-serif;
+    #root .container .foreground button {
+        font-family: 'Century Gothic', sans-serif;
         font-size: 14px;
         text-decoration: none;
         text-transform: uppercase;
-        background: #189cf0;
         display: inline-block;
         padding: 16px 38px;
-        border: 2px solid transparent;
-        border-radius: 40px;
-        color: #fff;
         font-weight: 400;
         -webkit-transition: 0.2s all;
         transition: 0.2s all;
     }
 
-    .error-403 a:hover {
-        background-color: #fff;
-        border-color: #189cf0;
-        color: #189cf0;
+    #root .container .foreground button.first {
+        background: #dc3545;
+        color: #fff;
     }
 
-    @media only screen and (max-width: 480px) {
-        .error-403 .notfound-404 h1 {
-            font-size: 162px;
-        }
+    #root .container .foreground button.first:hover {
+        background-color: #fff;
+        border-color: #dc3540;
+        color: #dc3545;
+    }
 
-        .error-403 h2 {
-            font-size: 26px;
-        }
+    #root .container .foreground button.second {
+        background: #28a745;
+        color: #fff;
+    }
+
+    #root .container .foreground button.second:hover {
+        background-color: #fff;
+        border-color: #28a740;
+        color: #28a745;
+    }
+
+    #root .container .foreground .footer {
+        margin-top: 48px;
+        text-align: center;
+        color: #fff;
+        background-color: #ffc107;
+        font-family: 'Century Gothic', sans-serif;
+        font-size: 14px;
+        font-weight: bold;
+        text-decoration: none;
+    }
+
+    .badge {
+        border: 2px solid transparent;
+        border-radius: 40px;
     }
 </style>
-<div id="parent">
-    <div id="error-403">
-        <div class="error-403">
+<div id="root">
+    <div class="container">
+        <div class="background">
             <div class="error-403">
                 <h1>403</h1>
             </div>
-            <h2>We are sorry, ACCESS DENIED!</h2>
-            <h4>You don't have sufficient privileges to access this data.</h4>
-            @if ($emergency_access)
-                <p>It seems like you can access this data in emergency access cases.</p>
-            @endif
-            <div>
-                <button class="btn btn-round btn-sm btn-success"
-                        onclick="window.history.back()"
-                >Back
-                </button>
+        </div>
+        <div class="foreground">
+            <div style="display: flex !important; flex-direction: column !important;">
+                <h2>We are sorry, ACCESS DENIED!</h2>
+                <h4>You don't have sufficient privileges to access this data.</h4>
                 @if ($emergency_access)
-                    <button class="btn btn-round btn-sm btn-primary"
-                            onclick="let req=new XMLHttpRequest();req.open('POST', 'localhost:5555/api/requests/{{$record_id}}/OK');req.send();localStorage.setItem('toggle', 'true');window.location.reload();
-                                    ">Emergency Access
-                    </button>
+                    <p>It seems like you can access this data in emergency access cases.</p>
                 @endif
+                <div>
+                    <button class="badge second"
+                            onclick="window.history.back()"
+                    >Back
+                    </button>
+                    @if ($emergency_access)
+                        <button class="badge first"
+                                onclick="let req=new XMLHttpRequest();req.open('POST', 'localhost:5555/api/requests/{{$record_id}}/OK');req.send();localStorage.setItem('toggle', 'true');window.location.reload();
+                                        ">Emergency Access
+                        </button>
+                    @endif
+                </div>
+
+                <hr/>
+
+                <div class="badge footer">
+                    Access to this site's data is controlled by <a href="http://localhost:5555">DMRAP</a> &copy;
+                    2019
+                </div>
             </div>
         </div>
     </div>
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -21,20 +21,53 @@
     <link href="{{ asset('css/styles.css') }}" rel="stylesheet"/>
 
     <style>
-        .form-control, .is-focused {
+        .form-control {
             background-image: linear-gradient(to top, transparent 2px, rgba(156, 39, 176, 0) 2px), linear-gradient(to top, #d2d2d2 1px, rgba(210, 210, 210, 0) 1px) !important;
         }
 
         .form-control:focus {
-            border-color: #999;
+            border-color: transparent;
         }
 
         .form-control {
-            border: 2px solid #999;
-            border-radius: 44px !important;
             overflow: hidden;
-            padding: 0 8px !important;
             font-size: 13px;
+        }
+
+        input {
+            border-radius: 100px !important;
+            padding: 10px 15px !important;
+            width: 50% !important;
+            border: 1px solid #D9D9D9 !important;
+            outline: none !important;
+            display: block !important;
+            margin: 20px auto 20px auto !important;
+        }
+
+        button {
+            border-radius: 100px !important;
+            border: none !important;
+            /*background: #719BE6 !important;*/
+            width: 50% !important;
+            padding: 10px !important;
+            color: #FFFFFF !important;
+            margin-top: 25px !important;
+            /*box-shadow: 0 2px 10px -3px #719BE6 !important;*/
+            display: block !important;
+            margin: 55px auto 10px auto !important;
+        }
+
+        a {
+            text-align: center !important;
+            margin-top: 30px !important;
+            /*color: #719BE6 !important;*/
+            text-decoration: none !important;
+            padding: 5px !important;
+            display: inline-block !important;
+        }
+
+        a:hover {
+            text-decoration: underline !important;
         }
     </style>
 </head>
@@ -45,55 +78,43 @@
         <nav class="navbar navbar-expand-lg navbar-transparent navbar-absolute fixed-top ">
             <div class="container-fluid">
                 <div class="navbar-wrapper mx-auto">
-                    <a class="navbar-brand brand-title" href="/">
-                        <span>Dynamic</span>
-                        <span>Medical</span>
-                        <br>
-                        <span>Records</span>
-                        <span>Access</span>
-                        <span>Policies</span>
+                    <a class="logo" href="/">
+                        <img src="{{ asset('img/logo.png') }}" class="img-fluid img-thumbnail">
                     </a>
                 </div>
             </div>
         </nav>
 
-        <div class="content">
+        <div class="content" style="margin-top: 150px">
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-md-6 mx-auto">
                         <div class="card">
                             <div class="card-header card-header-tabs card-header-success">
-                                {{--<div class="card-icon">--}}
-                                {{----}}
-                                {{--</div>--}}
                                 <h4>{{ __('Login') }}</h4>
                             </div>
-
                             <div class="card-body">
                                 <div class="row">
-                                    <div class="col-md-7 mx-auto py-3">
+                                    <div class="col-md-8 mx-auto py-3">
+
                                         <form method="POST" action="{{ route('login') }}">
                                             @csrf
 
-                                            <div class="form-group">
-                                                <input class="form-control"
-                                                       name="email"
-                                                       type="email"
-                                                       value="{{ old('email', '') }}"
-                                                       placeholder="{{ __('Email') }}"
-                                                       autocomplete="email"
-                                                       autofocus>
-                                            </div>
+                                            <input class="form-control"
+                                                   name="email"
+                                                   type="email"
+                                                   value="{{ old('email', '') }}"
+                                                   placeholder="{{ __('Email') }}"
+                                                   autocomplete="off"
+                                                   autofocus>
 
-                                            <div class="form-group">
-                                                <input class="form-control"
-                                                       name="password"
-                                                       type="password"
-                                                       value="{{ old('password', '') }}"
-                                                       placeholder="{{ __('Password') }}"
-                                                       autocomplete="email"
-                                                       autofocus>
-                                            </div>
+                                            <input class="form-control"
+                                                   name="password"
+                                                   type="password"
+                                                   value="{{ old('password', '') }}"
+                                                   placeholder="{{ __('Password') }}"
+                                                   autocomplete="off"
+                                                   autofocus>
 
                                             @if ($errors->any())
                                                 <div class="mt-5">
@@ -107,16 +128,15 @@
                                                 </div>
                                             @endif
 
-                                            <div class="form-group">
-                                                <button type="submit" class="btn btn-success">
-                                                    <i class="fa fa-sign-in-alt"></i>
-                                                    {{ __('Login') }}
-                                                </button>
-                                            </div>
+                                            <button type="submit" class="btn btn-success">
+                                                <i class="fa fa-sign-in-alt"></i>
+                                                {{ __('Login') }}
+                                            </button>
 
                                             <div class="text-center">
                                                 @if (Route::has('password.request'))
-                                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                                    <a class="btn btn-link text-success"
+                                                       href="{{ route('password.request') }}">
                                                         {{ __('Forgot Your Password?') }}
                                                     </a>
                                                 @endif
